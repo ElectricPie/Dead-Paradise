41321316a97b9dc891d3a4b55a86f93ea24bea36
Connor <Kon_Priest@Hotmail.com>
2023-05-31 14:31:39 +0100

    Added Unreal Top Down Template

---
+++ Source/DeadParadise/DeadParadise.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeadParadise.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DeadParadise, "DeadParadise" );
+
+DEFINE_LOG_CATEGORY(LogDeadParadise)
+ 
---
+++ Source/DeadParadise/DeadParadiseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DeadParadiseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADeadParadiseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADeadParadiseGameMode();
+};
+
+
+
