d17d1558347fd4731e5c215acecb885a2d6410b3
Connor <Kon_Priest@Hotmail.com>
2023-07-19 15:14:39 +0100

    Added description for the public PathfindingGrid methods

--- Source/DeadParadise/PathfindingGrid.h
+++ Source/DeadParadise/PathfindingGrid.h
@@ -24,11 +24,30 @@ protected:
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	float GetNodeRadius() const;
-	int GetGridSize() const;
 	
-	bool NodeFromWorldPoint(const FVector& WorldPosition, FPathingNode*& OutNode) const;
-	 * @brief Attempts to get the 
+	/**
+	 * @brief Returns the radius of the grids nodes
+	 * @return A float of the grids nodes size
+	 */
+	float GetNodeRadius() const;
+	/**
+	 * @brief Gets the number of nodes in the grid
+	 * @return An int with the number of nodes in the grid
+	 */
+	int GetGridSize() const;
+	/**
+	 * @brief Attempts to get the node with the closest world position to the WorldPosition provided
+	 * @param WorldPosition A FVector of the 
+	 * @param OutNode A FPathing Node pointer to store the output
+	 * @return Returns true if a node is found otherwise will return false
+	 */
+	bool NodeFromWorldPoint(const FVector& WorldPosition, OUT FPathingNode*& OutNode) const;
+	/**
+	 * @brief Attempts to get the node surrounding the provided node 
+	 * @param Node The node to check its neighbours
+	 * @param OutNeighboringNodes A Array to store the neighboring nodes
+	 * @return Returns false if something went wrong other wise will return true
+	 */
 	bool GetNeighbouringNodes(const FPathingNode& Node, OUT TArray<FPathingNode*>& OutNeighboringNodes);
 	
 private:
